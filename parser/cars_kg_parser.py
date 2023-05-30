27bb5bc772fd979e7e9af89f81dd1a4791957573
igor <igglike@gmail.com>
2023-05-30 17:59:21 +0600

    сделал парсер сайта cars.kg

---
+++ parser/cars_kg_parser.py
@@ -0,0 +1,25 @@
+"""Модуль для парсинга информации c сайта cars.kg"""
+import requests
+from bs4 import BeautifulSoup as BS
+
+
+URL = 'https://cars.kg/offers'
+page = 0
+while True:
+    page += 1
+    url = URL + f'/{page}' 
+    response = requests.get(url=URL, timeout=10)
+    # print(f"{response.status_code=}")
+    # print(f"{response.text[:270]=}")
+    if response.status_code == 200:
+        # проверка что не вышли за последнбб стр
+        end = 'По заданным критериям не найдено ни одного предложения'
+        
+        soup = BS(response.text, 'html.parser')
+        cars_list = soup.find('div', class_='catalog-list')
+        # print(cars_list.text[:200])
+        for c in cars_list.find_all('a', class_='catalog-list-item'):
+            model = c.find('span', class_='catalog-item-caption')
+            year = model.find(class_='caption-year').text
+            model = model.text.replace(f" , {year}", '').strip()
+            print(model)
